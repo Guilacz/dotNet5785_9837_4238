ec268f275a616d854758fae9f428cee8f8bb05e8
guilacz <guilacz03@gmail.com>
2024-10-31 15:17:16 +0200

    intial code

---
+++ dotNet5785_9837_4238/Stage0/Program9837.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Stage0 
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Welcome9837();
+            Welcome4238();
+            Console.ReadKey();
+
+
+        }
+        static partial void Welcome4238();
+        private static void Welcome9837()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first application", name);
+        }
+        
+
+    }
+}
+
