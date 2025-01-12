9fd860486ce1bf99bc4325e111cab037354aa128
guilacz <guilacz03@gmail.com>
2025-01-12 20:26:52 +0200

    enums

--- dotNet5785_9837_4238/PL/Enums.cs
+++ dotNet5785_9837_4238/PL/Enums.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace PL;
 internal class CallTypeCollection : IEnumerable
@@ -53,6 +56,43 @@ internal class CloseCallInListSortCollection : IEnumerable
     static readonly IEnumerable<BO.CloseCallInListSort> s_enums = (Enum.GetValues(typeof(BO.CloseCallInListSort)) as IEnumerable<BO.CloseCallInListSort>)!;
 
     public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+
+class ConvertCallTypeToColor : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var callType = (BO.CallType)value;
+
+        switch (callType)
+        {
+            case BO.CallType.Math_Primary:
+                return Brushes.Blue;
+            case BO.CallType.Math_Middle:
+                return Brushes.Cyan;
+            case BO.CallType.Math_High:
+                return Brushes.DarkBlue;
+            case BO.CallType.English_Primary:
+                return Brushes.LightGreen;
+            case BO.CallType.English_Middle:
+                return Brushes.Lime;
+            case BO.CallType.English_High:
+                return Brushes.Green;
+            case BO.CallType.Grammary_Primary:
+                return Brushes.Pink;
+            case BO.CallType.Grammary_Middle:
+                return Brushes.Purple;
+            case BO.CallType.Grammary_High:
+                return Brushes.Magenta;
+            default:
+                return Brushes.White; // default color if none match
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
 }
 internal class Enums
 {
